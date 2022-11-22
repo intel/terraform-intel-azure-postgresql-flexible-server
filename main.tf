a861066599e431836537a76feb457cee7ad4425d
kalenarndt <kalenarndt@gmail.com>
2022-11-22 12:22:47 -0700

    module changes

--- main.tf
+++ main.tf
@@ -1,47 +1,101 @@
 locals {
-  pgsql_config = var.pgsql_configuration != null ? { for k, v in var.pgsql_configuration : k => v if v != null } : {}
+  # Determine if the db_engine is set to mysql. If true then create a list of all the backup strings, if false then create an empty list. This list is referenced to determine the length which acts like a flag for the dynamic block
+  ha_flag = var.db_engine == "postgres" ? compact([var.db_ha_standby_zone, var.db_ha_mode]) : []
+
+  # Evaluates if a parameter wasn't supplied in the input map (someone didn't want to use it) and returns only the objects that have been configured
+  db_parameters = { for parameter, value in lookup(var.db_parameters, var.db_engine, {}) : parameter => value if value != null /* object */ }
+
+  # Determine if the db_engine is set to mysql. If true then create a list of all the maintenance strings, if false then create an empty list. This list is referenced to determine the length which acts like a flag for the dynamic block
+  maintenance_flag = var.db_engine == "postgres" ? compact([var.db_maintenance_day, var.db_maintenance_hour, var.db_maintenance_minute]) : []
+
+
+  # Firewall rules that are converted from a list of a map. Setting the name of the rule to the key and reconstructing it so we can use for_each instead of count
+  firewall_rules = {
+    for rule, value in var.db_firewall_rules : value.name => {
+      start_ip_address = value.start_ip_address
+      end_ip_address   = value.end_ip_address
+    }
+  }
 }
 
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-resource "azurerm_postgresql_flexible_server" "postgresql_flexible" {
-  name                   = var.pgsql_server_name
+resource "azurerm_postgresql_flexible_server" "postgres" {
+  name                   = var.db_server_name
   resource_group_name    = data.azurerm_resource_group.rg.name
   location               = data.azurerm_resource_group.rg.location
-  version                = var.pgsql_version
-  administrator_login    = var.pgsql_administrator_login
-  administrator_password = var.pgsql_administrator_login_password
-  create_mode            = var.create_mode
-  sku_name               = var.pgsql_server_sku
-  storage_mb             = var.storage_mb
-  tags = merge(
-    var.tags,
-    {
-      "Intel Cloud Optimization Module" = "Azure PostgreSQL Flexible"
+  version                = var.db_engine_version
+  administrator_login    = var.db_username
+  administrator_password = var.db_password
+  create_mode            = var.db_create_mode
+  sku_name               = var.db_server_sku
+  storage_mb             = var.db_allocated_storage
+  tags                   = var.tags
+
+  # Backups
+  backup_retention_days        = var.db_backup_retention_period
+  geo_redundant_backup_enabled = var.db_geo_backup_enabled
+
+  zone                = var.db_zone
+  private_dns_zone_id = var.db_private_dns_zone_id
+  delegated_subnet_id = var.db_private_dns_zone_id != null ? var.db_delegated_subnet_id : null
+
+
+  source_server_id                  = var.db_create_source_id
+  point_in_time_restore_time_in_utc = var.db_create_mode == "PointInTimeRestore" && var.db_create_source_id != null ? var.db_restore_time : null
+
+  dynamic "maintenance_window" {
+    for_each = length(local.maintenance_flag) >= 1 ? { "maintenance_flag" : "" } : {}
+    content {
+      day_of_week  = var.db_maintenance_day
+      start_hour   = var.db_maintenance_hour
+      start_minute = var.db_maintenance_minute
     }
-  )
+  }
+
+  dynamic "high_availability" {
+    for_each = length(local.ha_flag) >= 1 ? { "ha_flag" : "" } : {}
+    content {
+      mode                      = var.db_ha_mode
+      standby_availability_zone = var.db_ha_standby_zone
+    }
+  }
+
+  timeouts {
+    create = var.db_timeouts.create
+    delete = var.db_timeouts.delete
+    update = var.db_timeouts.update
+  }
+
 }
 
-resource "azurerm_postgresql_flexible_server_database" "postgresql_flexible_db" {
-  name      = var.pgsql_db_name
-  server_id = azurerm_postgresql_flexible_server.postgresql_flexible.id
-  collation = "en_US.utf8"
-  charset   = "utf8"
+resource "azurerm_postgresql_flexible_server_database" "postgres" {
+  count     = var.db_name != null ? 1 : 0
+  name      = var.db_name
+  server_id = azurerm_postgresql_flexible_server.postgres.id
+  collation = var.db_collation
+  charset   = var.db_charset
+
+  timeouts {
+    create = var.db_timeouts.create
+    delete = var.db_timeouts.delete
+    read   = var.db_timeouts.read
+  }
 }
 
-resource "azurerm_postgresql_flexible_server_firewall_rule" "example" {
-  count            = length(var.firewall_ip_range) == 0 ? 0 : length(var.firewall_ip_range)
-  name             = "pgsql_firewall_rule${count.index}"
-  server_id        = azurerm_postgresql_flexible_server.postgresql_flexible.id
-  start_ip_address = var.firewall_ip_range[count.index]["start_ip_address"]
-  end_ip_address   = var.firewall_ip_range[count.index]["end_ip_address"]
+resource "azurerm_postgresql_flexible_server_firewall_rule" "firewall" {
+  for_each         = local.firewall_rules
+  name             = each.key
+  server_id        = azurerm_postgresql_flexible_server.postgres.id
+  start_ip_address = each.value.start_ip_address
+  end_ip_address   = each.value.end_ip_address
 }
 
 resource "azurerm_postgresql_flexible_server_configuration" "pgsql_server_config" {
-  for_each  = local.pgsql_config
+  for_each  = local.db_parameters
   name      = each.key
-  server_id = azurerm_postgresql_flexible_server.postgresql_flexible.id
-  value     = each.value
+  server_id = azurerm_postgresql_flexible_server.postgres.id
+  value     = each.value.value
 }
