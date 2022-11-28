4d543c9a5d2d0082ae2dc698786f31804a8324fa
Lucas Melo <lucas.melo@intel.com>
2022-11-28 12:10:08 -0600

    Module Refactor
    * module changes
    
    * module changes
    
    * change resource name on line 96
    
    * terraform-docs: automated action
    
    Co-authored-by: kalenarndt <kalenarndt@gmail.com>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/intel-optimized-pgsql-flexible-server/main.tf
+++ examples/intel-optimized-pgsql-flexible-server/main.tf
@@ -1,22 +1,25 @@
-# Provision Intel Optimized Azure PostgreSQL server 
-module "optimized-pgsql-server" {
-  source                             = "../../"                                                #add the github url later
-  resource_group_name                = "shreejan_test_mssql"
-  pgsql_server_name                  = "optimized-pgsql-server-test"
-  pgsql_db_name                      = "optimized-pgsql-db-test"
-  pgsql_administrator_login_password = var.pgsql_administrator_login_password
-  tags                               = {
-                                            "duration" = 2,
-                                            "owner"    = "shreejan.mistry@intel.com"
-                                       }
-
-  firewall_ip_range                  =  [
-                                            {start_ip_address = "192.55.54.53",end_ip_address = "192.55.54.53" },
-                                            {start_ip_address = "250.151.84.219",end_ip_address = "250.151.84.219" },
-                                            {start_ip_address = "79.4.142.148",end_ip_address = "79.4.142.148"}
-                                         ]
+module "optimized-postgres-server" {
+  source              = "../../"
+  resource_group_name = "<YOUR-RG-ID>"              # Required
+  db_server_name      = "optimized-postgres-server" #  Required
+  db_password         = var.db_password             # Required
+  db_ha_mode          = "ZoneRedundant"             # Optional
+  db_name             = "test-db"                   # Optional
+  tags = {
+    name    = "name"
+    purpose = "intel"
+  }
+  # Optional but I need someone to do testing with these.
+  db_firewall_rules = [
+    {
+      end_ip_address   = "0.0.0.0"
+      name             = "Azure-all-services"
+      start_ip_address = "0.0.0.0"
+    },
+    {
+      end_ip_address   = "0.0.0.0"
+      name             = "Test-Rule"
+      start_ip_address = "0.0.0.0"
+    }
+  ]
 }
-
-#terraform init  
-#terraform plan -var="pgsql_administrator_login_password=..." #Enter a complex password
-#terraform apply -var="pgsql_administrator_login_password=..." #Enter a complex password
--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 ####     Intel      ####
 ########################
 
-# See policies.md, we recomment the Eds_v4-series running on the 3rd Generation Intel® Xeon® Platinum 8370C (Ice Lake), the Intel® Xeon® Platinum 8272CL (Cascade Lake) processors.
+# See policies.md, Intel recommends the Eds_v4-series running on the 3rd Generation Intel® Xeon® Platinum 8370C (Ice Lake), the Intel® Xeon® Platinum 8272CL (Cascade Lake) processors.
 # General Purpose: GP_Standard_E2ds_v4, GP_Standard_E4ds_v4, GP_Standard_E8ds_v4, GP_Standard_E16ds_v4, GP_Standard_E20ds_v4, GP_Standard_E32ds_v4,GP_Standard_E48ds_v4, GP_Standard_E64ds_v4
 # Memory Optimized: MO_Standard_E2ds_v4, MO_Standard_E4ds_v4, MO_Standard_E8ds_v4, MO_Standard_E16ds_v4, MO_Standard_E20ds_v4, MO_Standard_E32ds_v4,MO_Standard_E48ds_v4, MO_Standard_E64ds_v4
 # The number between E and d in MO_Standard_E8ds_v4 stands for vCores. 
@@ -10,72 +10,154 @@
 # See more:
 # https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/concepts-compute-storage
 
-variable "pgsql_server_sku" {
-  description = "The SKU Name for the PostgreSQL Flexible Server. The name of the SKU, follows the tier + name pattern (e.g. B_Standard_B1ms, GP_Standard_D2s_v3, MO_Standard_E4s_v3)."
-  default     = "MO_Standard_E8ds_v4"
+variable "db_server_sku" {
+  description = "Instance SKU, see comments above for guidance"
+  type        = string
+  default     = "MO_Standard_E4ds_v4"
 }
 
-# Intel Cloud Optimization Module for PostgreSQL optimizations  
-variable "pgsql_configuration" {
-  description = "PostgreSQL Server Optimizations"
-  type        = map(string)
+variable "db_parameters" {
+  type = object({
+    postgres = optional(object({
+      max_connections = optional(object({
+        value = optional(string, "256")
+      }))
+      # Instance issues # We need to add some form of logic to determine the percentage of the memory on the instance to allocate
+      # shared_buffers = optional(object({
+      #   value = optional(string, "64000")
+      # }))
+      huge_pages = optional(object({
+        value = optional(string, "on")
+      }))
+      temp_buffers = optional(object({
+        value = optional(string, "4000")
+      }))
+      work_mem = optional(object({
+        value = optional(string, "2097151")
+      }))
+      maintenance_work_mem = optional(object({
+        value = optional(string, "512000")
+      }))
+      autovacuum_work_mem = optional(object({
+        value = optional(string, "-1")
+      }))
+      effective_io_concurrency = optional(object({
+        value = optional(string, "32")
+      }))
+      wal_level = optional(object({
+        value = optional(string, "logical")
+      }))
+      wal_buffers = optional(object({
+        value = optional(string, "512")
+      }))
+      cpu_tuple_cost = optional(object({
+        value = optional(string, "0.03")
+      }))
+      effective_cache_size = optional(object({
+        value = optional(string, "350000000")
+      }))
+      random_page_cost = optional(object({
+        value = optional(string, "1.1")
+      }))
+      checkpoint_timeout = optional(object({
+        value = optional(string, "3600")
+      }))
+      checkpoint_completion_target = optional(object({
+        value = optional(string, "0.9")
+      }))
+      checkpoint_warning = optional(object({
+        value = optional(string, "1")
+      }))
+      log_min_messages = optional(object({
+        value = optional(string, "error")
+      }))
+      log_min_error_statement = optional(object({
+        value = optional(string, "error")
+      }))
+      autovacuum = optional(object({
+        value = optional(string, "on")
+      }))
+      autovacuum_max_workers = optional(object({
+        value = optional(string, "10")
+      }))
+      autovacuum_vacuum_cost_limit = optional(object({
+        value = optional(string, "3000")
+      }))
+      datestyle = optional(object({
+        value = optional(string, "ISO, DMY")
+      }))
+      lc_monetary = optional(object({
+        value = optional(string, "en_US.utf-8")
+      }))
+      lc_numeric = optional(object({
+        value = optional(string, "en_US.utf-8")
+      }))
+      default_text_search_config = optional(object({
+        value = optional(string, "pg_catalog.english")
+      }))
+      max_locks_per_transaction = optional(object({
+        value = optional(string, "64")
+      }))
+      max_wal_senders = optional(object({
+        value = optional(string, "5")
+      }))
+      min_wal_size = optional(object({
+        value = optional(string, "8192")
+      }))
+      max_wal_size = optional(object({
+        value = optional(string, "524")
+      }))
+    }))
+  })
   default = {
-    "max_connections"              = 256,
-    "shared_buffers"               = 64000,
-    "huge_pages"                   = "on",
-    "temp_buffers"                 = 4000,
-    "work_mem"                     = 2097151,
-    "maintenance_work_mem"         = 512000,
-    "autovacuum_work_mem"          = -1,
-    "effective_io_concurrency"     = 32,
-    "wal_level"                    = "logical",
-    "wal_buffers"                  = 512,
-    "cpu_tuple_cost"               = 0.03,
-    "effective_cache_size"         = 350000000,
-    "random_page_cost"             = 1.1,
-    "checkpoint_timeout"           = 3600,
-    "checkpoint_completion_target" = 0.9,
-    "checkpoint_warning"           = 1,
-    "log_min_messages"             = "error",
-    "log_min_error_statement "     = "error",
-    "autovacuum"                   = "on",
-    "autovacuum_max_workers"       = 10,
-    "autovacuum_vacuum_cost_limit" = 3000,
-    "datestyle"                    = "ISO, DMY",
-    "lc_monetary"                  = "en_US.UTF-8",
-    "lc_numeric"                   = "en_US.UTF-8",
-    "default_text_search_config"   = "pg_catalog.english",
-    "max_locks_per_transaction"    = 64,
-    "max_wal_senders"              = 5,
-    "min_wal_size"                 = 8192,
-    "max_wal_size"                 = 524
+    postgres = {
+      autovacuum                   = {}
+      autovacuum_max_workers       = {}
+      autovacuum_vacuum_cost_limit = {}
+      autovacuum_work_mem          = {}
+      checkpoint_completion_target = {}
+      checkpoint_timeout           = {}
+      checkpoint_warning           = {}
+      cpu_tuple_cost               = {}
+      datestyle                    = {}
+      default_text_search_config   = {}
+      effective_cache_size         = {}
+      effective_io_concurrency     = {}
+      huge_pages                   = {}
+      lc_monetary                  = {}
+      lc_numeric                   = {}
+      log_min_error_statement      = {}
+      log_min_messages             = {}
+      maintenance_work_mem         = {}
+      max_connections              = {}
+      max_locks_per_transaction    = {}
+      max_wal_senders              = {}
+      max_wal_size                 = {}
+      min_wal_size                 = {}
+      random_page_cost             = {}
+      # shared_buffers               = {} # We need to add some form of logic to determine the percentage of the memory on the instance to allocate
+      temp_buffers = {}
+      wal_buffers  = {}
+      wal_level    = {}
+      work_mem     = {}
+    }
+  }
+  description = "Intel Cloud optimizations for Xeon processors"
+  /* This parameter is READ-Only in Azure Portal and defaults to ON. */
 
+  # "synchronous_commit"   = "on",
 
-    /* This parameter is READ-Only in Azure Portal and defaults to ON. */
-
-    # "synchronous_commit"   = "on",   
-
-    /* The set of PostgreSQL configuration parameter are available and recommended on the Xeon Tunning Guide,
+  /* The set of PostgreSQL configuration parameter are available and recommended on the Xeon Tunning Guide,
         but as of right now, configuration of this parameters ((given below) is not supported. */
 
-    # "max_stack_depth"     = 7,
-    # "dynamic_shared_memory_type" = "posix",
-    # "max_files_per_process" = 4000,
-    # "max_pred_locks_per_transaction" = 64,
-    # "archive_mode" = "off",
-    # "lc_time" = "en_US.UTF-8",
-    # "lc_messages" = "en_US.UTF-8",
-  }
-
+  # "max_stack_depth"     = 7,
+  # "dynamic_shared_memory_type" = "posix",
+  # "max_files_per_process" = 4000,
+  # "max_pred_locks_per_transaction" = 64,
+  # "archive_mode" = "off",
+  # "lc_time" = "en_US.UTF-8",
+  # "lc_messages" = "en_US.UTF-8",
 }
-
-#PostgreSQL Server admin password. Do not commit password to version control systems 
-variable "pgsql_administrator_login_password" {
-  description = "PostgreSQL server name admin password"
-  type        = string
-  sensitive   = true
-}
-
 
 #Resource Group Name
 variable "resource_group_name" {
@@ -83,59 +165,186 @@ variable "resource_group_name" {
   type        = string
 }
 
-#PostgreSQL Server Name 
-variable "pgsql_server_name" {
-  description = "PostgreSQL server name"
+variable "db_username" {
+  description = "Username for the master database user."
   type        = string
+  default     = "pgadmin"
 }
 
-#PostgreSQL Database Name 
-variable "pgsql_db_name" {
-  description = "PostgreSQL Database name"
+variable "db_password" {
+  description = "Password for the master database user."
   type        = string
+  sensitive   = true
+  validation {
+    condition     = length(var.db_password) >= 8
+    error_message = "The db_password value must be at least 8 characters in length."
+  }
 }
 
-#The version of PostgreSQL Flexible Server to use. Possible values are 11,12, 13 and 14. Required when create_mode is Default. Changing this forces a new PostgreSQL Flexible Server to be created.
-variable "pgsql_version" {
-  description = "PostgreSQL Version"
+variable "db_zone" {
+  description = "Specifies the Availability Zone in which this Flexible Server should be located. Possible values are 1, 2 and 3."
   type        = string
-  default = "14"
+  default     = "2"
 }
 
-#PostgreSQL Server admin username 
-variable "pgsql_administrator_login" {
-  description = "PostgreSQL server name admin username"
+variable "db_ha_standby_zone" {
+  description = "Specifies the Availability Zone in which the standby Flexible Server should be located. Possible values are 1, 2 and 3."
   type        = string
-  default     = "pgsqladmin"
+  default     = "1"
 }
 
-#Mode of creation for PostgreSQL Server
-variable "create_mode" {
-  description = "The creation mode which can be used to restore or replicate existing servers. Possible values are Default and PointInTimeRestore. Changing this forces a new PostgreSQL Flexible Server to be created."
+variable "db_ha_mode" {
+  description = "The high availability mode for the Flexible Server. Possibles values are ZoneRedundant."
   type        = string
-  default     = "Default"
+  default     = "ZoneRedundant"
 }
 
-#Capacity of the PostgreSQL
-variable "storage_mb" {
+# Backups
+variable "db_backup_retention_period" {
+  description = "The days to retain backups for. Must be between 1 and 35."
+  type        = number
+  validation {
+    condition     = var.db_backup_retention_period >= 1 && var.db_backup_retention_period <= 35
+    error_message = "The db_backup_retention_period must be null or if specified between 7 and 35."
+  }
+  default = 7
+}
+
+variable "db_geo_backup_enabled" {
+  description = "Turn Geo-redundant server backups on/off. This allows you to choose between locally redundant or geo-redundant backup storage in the General Purpose and Memory Optimized tiers. When the backups are stored in geo-redundant backup storage, they are not only stored within the region in which your server is hosted, but are also replicated to a paired data center. This provides better protection and ability to restore your server in a different region in the event of a disaster"
+  type        = bool
+  default     = false
+}
+
+variable "db_allocated_storage" {
   description = "The max storage allowed for the PostgreSQL Flexible Server. Possible values (MB) are 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, and 16777216."
   type        = number
   default     = 32768
 }
 
-#Tags
+variable "db_create_mode" {
+  description = "The creation mode which can be used to restore or replicate existing servers."
+  type        = string
+  validation {
+    condition     = contains(["Default", "Replica", "GeoRestore", "PointInTimeRestore"], var.db_create_mode)
+    error_message = "The db_create_mode must be \"Default\",\"Replica\",\"GeoRestore\", or \"PointInTimeRestore\"."
+  }
+  default = "Default"
+}
+
+variable "db_create_source_id" {
+  description = "For creation modes other than Default, the source server ID to use."
+  type        = string
+  default     = null
+}
+
+variable "db_name" {
+  description = "Name of the database that will be created on the flexible instance. If this is specified then a database will be created as a part of the instance provisioning process."
+  type        = string
+  default     = null
+}
+
+#TODO Check with Lucas on this validation
+variable "db_engine_version" {
+  description = "Database engine version for the Azure database instance."
+  type        = string
+  validation {
+    condition     = contains(["11", "12", "13", "14"], var.db_engine_version)
+    error_message = "The db_engine_version must be \"11\", \"12\", \"13\" or \"14\"."
+  }
+  default = "14"
+}
+
+variable "db_restore_time" {
+  description = "When create_mode is PointInTimeRestore, specifies the point in time to restore from creation_source_server_id. It should be provided in RFC3339 format, e.g. 2013-11-08T22:00:40Z."
+  type        = string
+  default     = null
+}
+
+variable "db_server_name" {
+  description = "Name of the server that will be created."
+  type        = string
+}
+
+
+variable "db_collation" {
+  description = "Specifies the Collation for the Database."
+  type        = string
+  default     = "en_US.UTF8"
+}
+
+variable "db_charset" {
+  description = "Specifies the Charset for the database."
+  type        = string
+  default     = "UTF8"
+}
+
+variable "db_private_dns_zone_id" {
+  description = "The ID of the private DNS zone to create the Flexible Server."
+  type        = string
+  default     = null
+}
+
+variable "db_delegated_subnet_id" {
+  description = "The ID of the virtual network subnet to create the Flexible Server."
+  type        = string
+  default     = null
+}
+
+variable "db_timeouts" {
+  type = object({
+    create = optional(string, null)
+    delete = optional(string, null)
+    update = optional(string, null)
+    read   = optional(string, null)
+  })
+  description = "Map of timeouts that can be adjusted when executing the module. This allows you to customize how long certain operations are allowed to take before being considered to have failed."
+  default = {
+    db_timeouts = {}
+  }
+}
+
+variable "db_engine" {
+  description = "Database engine for Azure database instance."
+  type        = string
+  validation {
+    condition     = contains(["postgres"], var.db_engine)
+    error_message = "The db_engine must be \"postgres\"."
+  }
+  default = "postgres"
+}
+
+
 variable "tags" {
-  description = "Tags"
+  description = "Tags to apply to the Database Server"
   type        = map(string)
   default     = {}
 }
 
-#For example:  [{start_ip_address = ..., end_ip_address = ... },..]"
-variable "firewall_ip_range" {
+variable "db_maintenance_day" {
+  description = "The day of week for maintenance window."
+  type        = string
+  default     = null
+}
+
+variable "db_maintenance_hour" {
+  description = "The start hour for maintenance window."
+  type        = string
+  default     = null
+}
+
+variable "db_maintenance_minute" {
+  description = "The start minute for maintenance window."
+  type        = string
+  default     = null
+}
+
+variable "db_firewall_rules" {
+  description = "Map of IP ranges that (if specified) will create firewall rules for the server to access those addresses."
   type = list(object({
-    start_ip_address = string
-    end_ip_address   = string
+    name             = string
+    start_ip_address = optional(string)
+    end_ip_address   = optional(string)
   }))
-  description = "User will provide range of IP addrress in form of List of (objects)"
-  default     = []
+  default = []
 }
